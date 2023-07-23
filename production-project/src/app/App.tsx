8ef93d4063fc1a189f4522373b8d7141ed193e0f
itsaln <itsaln23@gmail.com>
2023-07-24 01:18:44 +0500

    18

--- production-project/src/app/App.tsx
+++ production-project/src/app/App.tsx
@@ -1,15 +1,14 @@
 import { FC, Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
 
 import { classNames } from 'shared/lib/classNames/classNames'
 
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from 'app/providers/router'
 
-import './styles/index.scss'
-
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
+
+import './styles/index.scss'
 
 const App: FC = () => {
 	const { theme } = useTheme()
